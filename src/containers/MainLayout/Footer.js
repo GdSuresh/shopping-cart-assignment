32b4885964097c26a1ab4840a4a781f6e1643961
GdSuresh <102229521+GdSuresh@users.noreply.github.com>
2022-03-29 18:25:31 +0530

    Gaddam Suresh - Assingnment submission

---
+++ src/containers/MainLayout/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="d-flex justify-content-center">
+      <span>
+        Copyright &copy; 2022-2025 Sabka Bazaar Grocery Supplies Pvt Ltd.
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
